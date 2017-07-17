982a2879837d2d93f4ccb397906b0e7d9bb90f26
Phillip Nzuli <mwanikiphillip@gmail.com>
2017-07-17 17:03:33 +0300

    Added business logic to return all topics

---
+++ src/main/java/ke/co/philsoft/controllers/TopicsController.java
@@ -0,0 +1,25 @@
+package ke.co.philsoft.controllers;
+
+import ke.co.philsoft.models.Topic;
+import ke.co.philsoft.services.TopicsService;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+import java.util.Arrays;
+import java.util.List;
+
+/**
+ * Created by Phillip Mwaniki on 17-Jul-17.
+ */
+@RestController
+public class TopicsController {
+
+    @Autowired
+    private TopicsService topicsService;
+
+    @RequestMapping("/topics")
+    public List<Topic> getAllTopics() {
+        return topicsService.getAllTopics();
+    }
+}
---
+++ src/main/java/ke/co/philsoft/services/TopicsService.java
@@ -0,0 +1,27 @@
+package ke.co.philsoft.services;
+
+import ke.co.philsoft.models.Topic;
+import org.springframework.stereotype.Service;
+
+import java.util.Arrays;
+import java.util.List;
+
+/**
+ * Created by Phillip Mwaniki on 17-Jul-17.
+ */
+@Service
+public class TopicsService {
+
+    private List<Topic> topics() {
+        return Arrays.asList(
+                new Topic("spring", "Spring Framework", "lorem"),
+                new Topic("java", "Core Java", "lorem2"),
+                new Topic("javascript", "Javascript", "lorem3")
+        );
+    }
+
+   public List<Topic> getAllTopics() {
+        return topics();
+   }
+
+}
