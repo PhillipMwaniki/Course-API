4da447fd235579ff6a0bd360860a7cab76c01944
Phillip Nzuli <mwanikiphillip@gmail.com>
2017-07-18 09:45:36 +0300

    Implemented all CRUD functions for Topic service

--- src/main/java/ke/co/philsoft/controllers/TopicsController.java
+++ src/main/java/ke/co/philsoft/controllers/TopicsController.java
@@ -3,11 +3,8 @@ package ke.co.philsoft.controllers;
 import ke.co.philsoft.models.Topic;
 import ke.co.philsoft.services.TopicsService;
 import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.web.bind.annotation.PathVariable;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.*;
 
-import java.util.Arrays;
 import java.util.List;
 
 /**
@@ -28,5 +25,32 @@ public class TopicsController {
     public Topic getTopic(@PathVariable String id) {
 
         return topicsService.getTopic(id);
+    }
+
+    @RequestMapping(value = "/topics", method= RequestMethod.POST)
+    public List<Topic> addTopic(@RequestBody Topic topic) {
+
+        topicsService.addTopic(topic);
+        return topicsService.getAllTopics();
+    }
+
+    /**
+     *
+     * @param topic
+     * @param id
+     * @return
+     */
+    @RequestMapping(value = "/topics/{id}", method= RequestMethod.PUT)
+    public List<Topic> update(@RequestBody Topic topic, @PathVariable String id) {
+
+        topicsService.update(id, topic);
+        return topicsService.getAllTopics();
+    }
+
+    @RequestMapping(value = "/topics/{id}", method= RequestMethod.DELETE)
+    public List<Topic> delete(@PathVariable String id) {
+
+        topicsService.delete(id);
+        return topicsService.getAllTopics();
     }
 }
--- src/main/java/ke/co/philsoft/services/TopicsService.java
+++ src/main/java/ke/co/philsoft/services/TopicsService.java
@@ -3,6 +3,7 @@ package ke.co.philsoft.services;
 import ke.co.philsoft.models.Topic;
 import org.springframework.stereotype.Service;
 
+import java.util.ArrayList;
 import java.util.Arrays;
 import java.util.List;
 
@@ -12,20 +13,36 @@ import java.util.List;
 @Service
 public class TopicsService {
 
-    private List<Topic> topics() {
-        return Arrays.asList(
+    private List<Topic> topics = new ArrayList<>(Arrays.asList(
                 new Topic("spring", "Spring Framework", "lorem"),
                 new Topic("java", "Core Java", "lorem2"),
                 new Topic("javascript", "Javascript", "lorem3")
-        );
-    }
+        ));
 
    public List<Topic> getAllTopics() {
-        return topics();
+        return topics;
    }
 
    public Topic getTopic(String id) {
-       return topics().stream().filter(t -> t.getId().equals(id)).findFirst().get();
+       return topics.stream().filter(t -> t.getId().equals(id)).findFirst().get();
    }
 
+    public void addTopic(Topic topic) {
+        topics.add(topic);
+    }
+
+    public void update(String id, Topic topic) {
+
+       for (int i = 0; i<topics.size(); i++) {
+           Topic t = topics.get(i);
+           if (t.getId().equals(id)) {
+               topics.set(i, topic);
+               return;
+           }
+       }
+    }
+
+    public void delete(String id) {
+       topics.removeIf(t -> t.getId().equals(id));
+    }
 }
